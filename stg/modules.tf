95d1eba46ecabde34a8c3bdb6e6e289eae1622a1
cecuevas-hblf <carloscue-c@herbalife.com>
2025-01-14 14:01:17 -0600

    Update modules.tf

--- stg/modules.tf
+++ stg/modules.tf
@@ -1,11 +1,11 @@
 module "network" {
   source  = "terraform-google-modules/network/google"
-  version = "10.0.0"
+  version = "9.0.0"
   # insert the 3 required variables here
 }
 
 module "project-factory" {
   source  = "terraform-google-modules/project-factory/google"
-  version = "18.0.0"
+  version = "16.0.0"
   # insert the 2 required variables here
 }
