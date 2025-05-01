25d7839bbd49ce66c417b3a6414769c6c428f3e6
Carlos Cuevas <carloscue-c@herbalife.com>
2025-04-30 18:25:22 -0600

    Update module.tf

--- dev/akamai/module.tf
+++ dev/akamai/module.tf
@@ -1,1 +1,11 @@
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "8.0.0"
+  # insert the 3 required variables here
+}
 
+module "project-factory" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "17.0.0"
+  # insert the 2 required variables here
+}
